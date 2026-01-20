bf19a5dea101af4e70dd0c2bcf1a19bdf0214c52
osama <osamamoahmed242@gmail.com>
2026-01-21 00:40:11 +0200

    feat: Add ContactUsController and views for contact messages
    Implemented ContactUsController with full CRUD actions using NewsDbContext. Added Index, Details, and Delete Razor views for managing contact messages, including a contact form, message details, and deletion confirmation. Views styled with Bootstrap and custom CSS for improved user experience.

---
+++ NewsWire/Controllers/ContactUsController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsWire.Data;
+using NewsWire.Models;
+
+namespace NewsWire.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly NewsDbContext _context;
+
+        public ContactUsController(NewsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContactUs
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ContactUs/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactUs = _context.ContactUs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactUs == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactUs/Create To protect from overposting attacks, enable the specific
+        // properties you want to bind to. For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,Email,Subject,Message")] ContactUs contactUs)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactUs);
+                _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactUs = _context.ContactUs.Find(id);
+            if (contactUs == null)
+            {
+                return NotFound();
+            }
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactUs = _context.ContactUs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactUs == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactUs);
+        }
+
+        // POST: ContactUs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var contactUs = _context.ContactUs.Find(id);
+            if (contactUs != null)
+            {
+                _context.ContactUs.Remove(contactUs);
+            }
+
+            _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public bool ContactUsExists(int id)
+        {
+            return _context.ContactUs.Any(e => e.Id == id);
+        }
+    }
+}
