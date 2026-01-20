92a83e0bbf4569e4e1b0ae20a2002a050d7e6477
osama <osamamoahmed242@gmail.com>
2026-01-21 00:40:13 +0200

    Refactor :  HomeController and views; update navigation, add CSS
    Removed About, TeamMember, News, and SaveContact actions from HomeController. Deleted About.cshtml, Contact.cshtml, TeamMember.cshtml, and News.cshtml views. Rewrote Index.cshtml to include About, Stats, and Mission sections; removed category listing. Updated navigation in _Layout.cshtml to point Team Member and Contact to new controllers. Added crud-forms.css for improved form and card styling. Consolidated informational content and modernized UI structure.

--- NewsWire/Controllers/HomeController.cs
+++ NewsWire/Controllers/HomeController.cs
@@ -21,59 +21,17 @@ namespace NewsWire.Controllers
             return View(categories);
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
 
-        public IActionResult TeamMember()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult SaveContact(ContactUs contactUs)
-        {
-            if (ModelState.IsValid)
-            {
-                db.ContactUs.Add(contactUs);
-                db.SaveChanges();
-                return Content("OK");
-            }
-            return View(contactUs);
-        }
-
         public IActionResult Contact()
         {
             return View();
         }
-
-        public IActionResult News(int id, int page = 1)
-        {
-            const int pageSize = 8;
-            var news = db.News.Where(n => n.CategoryId == id);
-            var totalNews = news.Count();
-            var totalPages = (int)Math.Ceiling((double)totalNews / pageSize);
-
-            var newsList = news.OrderByDescending(n => n.PublishedAt)
-                         .Skip((page - 1) * pageSize)
-                         .Take(pageSize)
-                         .ToList();
-
-            ViewBag.CurrentPage = page;
-            ViewBag.TotalPages = totalPages;
-            ViewBag.CategoryId = id;
-            ViewBag.HasPreviousPage = page > 1;
-            ViewBag.HasNextPage = page < totalPages;
-
-            return View(newsList);
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
