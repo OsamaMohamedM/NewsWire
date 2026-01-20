f2dc41674a19e6ae8227bac93ca68643d2ea6623
osama <osamamoahmed242@gmail.com>
2026-01-21 00:40:11 +0200

    feat : Add CRUD controllers and views for News and TeamMembers
    Implemented NewsController and TeamMembersController with full CRUD operations using Entity Framework Core. Added Razor views for listing, creating, editing, viewing, and deleting news articles and team members. News listing supports pagination and category filtering; team view includes a static editorial team and careers section. All views styled with Bootstrap and custom CSS.

---
+++ NewsWire/Controllers/NewsController.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NewsWire.Data;
+using NewsWire.Models;
+
+namespace NewsWire.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly NewsDbContext _context;
+
+        public NewsController(NewsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: News
+
+        public IActionResult Index(int id, int page = 1)
+        {
+            int pageSize = 6;
+            var source = _context.News
+                                 .Where(n => n.CategoryId == id)
+                                 .Include(n => n.Category);
+            int totalItems = source.Count();
+            int totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.HasPreviousPage = page > 1;
+            ViewBag.HasNextPage = page < totalPages;
+            ViewBag.CategoryId = id;
+            var pagedNews = source.Skip((page - 1) * pageSize)
+                                  .Take(pageSize)
+                                  .ToList();
+
+            return View(pagedNews);
+        }
+
+        // GET: News/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = _context.News
+                .Include(n => n.Category)
+                .FirstOrDefault(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // GET: News/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Description");
+            return View();
+        }
+
+        // POST: News/Create To protect from overposting attacks, enable the specific properties you
+        // want to bind to. For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Title,Content,ImageUrl,PublishedAt,Topic,CategoryId")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(news);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Description", news.CategoryId);
+            return View(news);
+        }
+
+        // GET: News/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = _context.News.Find(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Description", news.CategoryId);
+            return View(news);
+        }
+
+        // POST: News/Edit/5 To protect from overposting attacks, enable the specific properties you
+        // want to bind to. For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Title,Content,ImageUrl,PublishedAt,Topic,CategoryId")] News news)
+        {
+            if (id != news.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(news);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NewsExists(news.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Description", news.CategoryId);
+            return View(news);
+        }
+
+        // GET: News/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = _context.News
+                .Include(n => n.Category)
+                .FirstOrDefault(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var news = _context.News.Find(id);
+            if (news != null)
+            {
+                _context.News.Remove(news);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NewsExists(int id)
+        {
+            return _context.News.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ NewsWire/Controllers/TeamMembersController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsWire.Data;
+using NewsWire.Models;
+
+namespace NewsWire.Controllers
+{
+    public class TeamMembersController : Controller
+    {
+        private readonly NewsDbContext _context;
+
+        public TeamMembersController(NewsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeamMembers
+        public IActionResult Index()
+        {
+            return View(_context.TeamMembers.ToList());
+        }
+
+        // GET: TeamMembers/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamMember = _context.TeamMembers
+                .FirstOrDefault(m => m.Id == id);
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(teamMember);
+        }
+
+        // GET: TeamMembers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TeamMembers/Create To protect from overposting attacks, enable the specific
+        // properties you want to bind to. For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,JobTitle,ImageUrl")] TeamMember teamMember)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teamMember);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teamMember);
+        }
+
+        // GET: TeamMembers/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamMember = _context.TeamMembers.Find(id);
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+            return View(teamMember);
+        }
+
+        // POST: TeamMembers/Edit/5 To protect from overposting attacks, enable the specific
+        // properties you want to bind to. For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name,JobTitle,ImageUrl")] TeamMember teamMember)
+        {
+            if (id != teamMember.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teamMember);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamMemberExists(teamMember.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(teamMember);
+        }
+
+        // GET: TeamMembers/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teamMember = _context.TeamMembers
+                .FirstOrDefault(m => m.Id == id);
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(teamMember);
+        }
+
+        // POST: TeamMembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var teamMember = _context.TeamMembers.Find(id);
+            if (teamMember != null)
+            {
+                _context.TeamMembers.Remove(teamMember);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamMemberExists(int id)
+        {
+            return _context.TeamMembers.Any(e => e.Id == id);
+        }
+    }
+}
